edece94ea7f6a08f551bbcbbf273ba27f577eafa
Turcew <K00fillia@gmail.com>
2026-04-22 22:14:24 +0300

    Update code

--- src/index.js
+++ src/index.js
@@ -7,18 +7,16 @@ function getExpression(target) {
     if (n === 1) {
       return "1";
     }
-
-    if (n > 5) {
-      const prev = findExpression(n - 5);
-      if (prev !== null) {
-        return `(${prev} + 5)`;
-      }
-    }
-
     if (n % 3 === 0) {
       const prev = findExpression(n / 3);
       if (prev !== null) {
         return `(${prev} * 3)`;
+      }
+    }
+    if (n > 5) {
+      const prev = findExpression(n - 5);
+      if (prev !== null) {
+        return `(${prev} + 5)`;
       }
     }
 
